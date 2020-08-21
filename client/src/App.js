6768769b5e4b4545f88a9de20bf32667af7a5522
oliverpdahl <oliverpdahl@gmail.com>
2020-08-21 10:33:16 -0400

    Switch out the client files

--- client/src/App.js
+++ client/src/App.js
@@ -1,107 +1,60 @@
+import { fetchCompletables } from "./actions/fetchCompletables";
+import { fetchPaths } from "./actions/fetchPaths";
+import CompletablesContainer from "./components/completables/CompletablesContainer";
 import React, { Component } from "react";
-import {
-  Container,
-  Header,
-  Segment,
-  Button,
-  Icon,
-  Dimmer,
-  Loader,
-  Divider,
-} from "semantic-ui-react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import NavBarContainer from "./components/navBar/NavBarContainer";
+import { Container, Row, Col } from "react-bootstrap";
+import AboutContainer from "./components/about/AboutContainer";
+import UserContainer from "./components/user/UserContainer";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {};
-    this.getDrinks = this.getDrinks.bind(this);
-    this.getDrink = this.getDrink.bind(this);
-  }
-
   componentDidMount() {
-    this.getDrinks();
+    this.props.fetchCompletables();
+    this.props.fetchPaths();
   }
 
-  fetch(endpoint) {
-    return window
-      .fetch(endpoint)
-      .then((response) => response.json())
-      .catch((error) => console.log(error));
-  }
-
-  getDrinks() {
-    this.fetch("/api/drinks").then((drinks) => {
-      if (drinks.length) {
-        this.setState({ drinks: drinks });
-        this.getDrink(drinks[0].id);
-      } else {
-        this.setState({ drinks: [] });
-      }
-    });
-  }
-
-  getDrink(id) {
-    this.fetch(`/api/drinks/${id}`).then((drink) =>
-      this.setState({ drink: drink })
-    );
-  }
+  handleLoading = () => {
+    if (this.props.loading) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <CompletablesContainer
+          localCompletables={this.props.paths}
+          allCompletables={this.props.completables}
+        />
+      );
+    }
+  };
 
   render() {
-    let { drinks, drink } = this.state;
-    return drinks ? (
-      <Container text>
-        <Header as="h2" icon textAlign="center" color="teal">
-          <Icon name="unordered list" circular />
-          <Header.Content>List of Ingredients</Header.Content>
-        </Header>
-        <Divider hidden section />
-        {drinks && drinks.length ? (
-          <Button.Group color="teal" fluid widths={drinks.length}>
-            {Object.keys(drinks).map((key) => {
-              return (
-                <Button
-                  active={drink && drink.id === drinks[key].id}
-                  fluid
-                  key={key}
-                  onClick={() => this.getDrink(drinks[key].id)}
-                >
-                  {drinks[key].title}
-                </Button>
-              );
-            })}
-          </Button.Group>
-        ) : (
-          <Container textAlign="center">No drinks found.</Container>
-        )}
-        <Divider section />
-        {drink && (
-          <Container>
-            <Header as="h2">{drink.title}</Header>
-            {drink.description && <p>{drink.description}</p>}
-            {drink.ingredients && (
-              <Segment.Group>
-                {drink.ingredients.map((ingredient, i) => (
-                  <Segment key={i}>{ingredient.description}</Segment>
-                ))}
-              </Segment.Group>
-            )}
-            {drink.steps && <p>{drink.steps}</p>}
-            {drink.source && (
-              <Button basic size="tiny" color="teal" href={drink.source}>
-                Source
-              </Button>
-            )}
-          </Container>
-        )}
-      </Container>
-    ) : (
-      <Container text>
-        <Dimmer active inverted>
-          <Loader content="Loading" />
-        </Dimmer>
-      </Container>
+    return (
+      <Router>
+        <Container fluid>
+          <Row className="fill">
+            <Col xs={0.5} className="fill">
+              <NavBarContainer />
+            </Col>
+            <Route exact path="/" render={() => <Redirect to="/paths" />} />
+            <Route path="/paths" render={() => this.handleLoading()} />
+            <Route path="/about" component={AboutContainer} />
+            <Route path="/user" component={UserContainer} />
+          </Row>
+        </Container>
+      </Router>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = (state) => {
+  return {
+    completables: state.completables,
+    paths: state.paths,
+    loading: state.loading,
+  };
+};
+
+export default connect(mapDispatchToProps, { fetchCompletables, fetchPaths })(
+  App
+);
