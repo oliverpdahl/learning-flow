1e3aa795c7eae708efdc388a45596549d226853a
oliverpdahl <oliverpdahl@gmail.com>
2020-08-21 08:40:59 -0400

    update application record for scaffold

--- flow/config/application.rb
+++ flow/config/application.rb
@@ -10,8 +10,6 @@ require 'active_record/railtie'
 require 'active_storage/engine'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
-require 'action_mailbox/engine'
-require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
 require 'sprockets/railtie'
@@ -21,7 +19,7 @@ require 'rails/test_unit/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Daloy
+module ListOfIngredients
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
