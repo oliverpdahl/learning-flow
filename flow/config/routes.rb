156e4ab7cf0a536c91771d078fdd79c64729d3dc
oliverpdahl <oliverpdahl@gmail.com>
2020-08-21 08:47:12 -0400

    update for scope api

--- flow/config/routes.rb
+++ flow/config/routes.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :ingredients
-  resources :drinks
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope '/api' do
+    resources :drinks
+  end
 end
