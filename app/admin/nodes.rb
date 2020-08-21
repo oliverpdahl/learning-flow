9e5d8e7243165d58a134e192518997b5f213258f
oliverpdahl <oliverpdahl@gmail.com>
2020-08-21 10:14:33 -0400

    Add nodes

---
+++ app/admin/nodes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Node do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :title, :complete
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :complete, :path_id, :type]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
--- db/migrate/20200626140852_create_content_blocks.rb
+++ db/migrate/20200626140852_create_content_blocks.rb
@@ -3,6 +3,7 @@
 class CreateContentBlocks < ActiveRecord::Migration[6.0]
   def change
     create_table :content_blocks do |t|
+      t.string :title
       t.integer :completable_id
 
       t.timestamps
